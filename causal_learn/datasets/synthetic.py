79694faeeb73c002d5dbce4494df51347c1e90c2
Guilherme Goto Escudero <guilhermegoto89@gmail.com>
2021-10-17 20:47:18 -0300

    clipping chance of treatment

--- causal_learn/datasets/synthetic.py
+++ causal_learn/datasets/synthetic.py
@@ -6,7 +6,7 @@ def create_synthetic_data(size=1000, random_seed=None):
         np.random.seed(random_seed)
     treatment_effect = 1
     x = np.random.normal(1, 1, size)
-    chance_of_treatment = 0.5 + x / 10
+    chance_of_treatment = np.clip(0.5 + x / 10, 0, 1)
     treatment = np.random.binomial(1, chance_of_treatment, size)
     y = 0.5 * x + treatment_effect * treatment
     return x.reshape(-1, 1), treatment, y
