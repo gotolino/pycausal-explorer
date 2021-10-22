625b20f774abad7031cbe8add11e16665f14098f
Guilherme Goto Escudero <guilhermegoto89@gmail.com>
2021-10-22 13:26:51 -0300

    removing placeholder files

--- test/test_hello.py
+++
@@ -1,9 +0,0 @@
-from causal_learn.hello import make_greeting
-
-
-def test_say_hello():
-    greeting = make_greeting()
-    greeting_name = make_greeting("John")
-
-    assert greeting == "Hello!"
-    assert greeting_name == "Hello, John!"
