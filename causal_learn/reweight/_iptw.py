c6e088b6a0e3cd8203ee8a01280a69a39950245b
Guilherme Goto Escudero <guilhermegoto89@gmail.com>
2021-10-29 13:43:59 -0300

    docs: docstring for propensity score

--- causal_learn/reweight/_iptw.py
+++ causal_learn/reweight/_iptw.py
@@ -7,17 +7,27 @@ from ._propensity_score import PropensityScore
 
 
 class IPTW(BaseEstimator):
+    """
+    Implements Inverse Probability Treatment Weighting (IPTW) model.
+
+    This model weights the units based on the propensity score. It uses all units to get the
+    average treatment effect, so all units receives the same ite.
+    Parameters
+    ----------
+    propensity_score: model used to calculate propensity score.
+    """
+
     def __init__(self, propensity_score=PropensityScore()):
         self.propensity_score = propensity_score
-        self._weight = None
+        self.weight_ = None
 
     def fit(self, X, w, y):
         self.propensity_score.fit(X, w)
         propensity_score_hat = self.propensity_score.predict_proba(X)[:, 1]
-        self._weight = (w - propensity_score_hat) / (
+        self.weight_ = (w - propensity_score_hat) / (
             propensity_score_hat * (1 - propensity_score_hat)
         )
-        self._ate = np.mean(self._weight * y)
+        self._ate = np.mean(self.weight_ * y)
 
     def predict(self, X, w):
         pass
--- causal_learn/reweight/_propensity_score.py
+++ causal_learn/reweight/_propensity_score.py
@@ -3,6 +3,18 @@ from sklearn.preprocessing import StandardScaler
 
 
 class PropensityScore:
+    """
+    Implements the Propensity Score model.
+
+    It fits a model with the target being the treatment, where the output is the probability of the individual being
+    treated.
+
+    Parameters
+    ----------
+    model: model used to calculate the propensity score
+    scaler: transformation used in the features space
+    """
+
     def __init__(self, model=LogisticRegression(), scaler=StandardScaler()):
         self.model = model
         self.scaler = scaler
@@ -10,6 +22,7 @@ class PropensityScore:
     def fit(self, X, w):
         X_scaled = self.scaler.fit_transform(X)
         self.model = self.model.fit(X_scaled, w)
+        return self
 
     def predict(self, X):
         X_scaled = self.scaler.transform(X)
