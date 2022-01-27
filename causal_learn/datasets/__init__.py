0146e0a317018c4cd75a66744b011ee15626f6b3
heitormsantos <heitorsantos.91@gmail.com>
2022-01-27 20:55:26 -0300

    feat: include lalonde jobs dataset

---
+++ causal_learn/datasets/__init__.py
@@ -0,0 +1,1 @@
+from ._lalonde_nsw_jobs import *
---
+++ causal_learn/datasets/_filenames.py
@@ -0,0 +1,7 @@
+from os import path
+
+
+ROOT_DIR = path.dirname(path.abspath(__file__))
+DATASETS_PATH = 'data'
+
+LALONDE_NSW_JOBS_CSV_FILE = 'nsw_jobs_lalonde_exp_psid_1978.csv'
