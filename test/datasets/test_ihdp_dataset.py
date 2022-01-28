b66ea36e52c3ed5d5cebaff1da5bc4cf90ca0a6d
heitormsantos <heitorsantos.91@gmail.com>
2022-01-27 23:54:28 -0300

    feat: include ihdp dataset

---
+++ test/datasets/test_ihdp_dataset.py
@@ -0,0 +1,17 @@
+from pycausal_explorer.datasets.ihdp_simulated_outcomes import load_ihdp_simulated_outcomes_dataset
+
+
+CSV_COLUMNS = columns_to_save = [
+    'treat', 'bw', 'b.head', 'preterm', 'birth.o', 'nnhealth', 'momage',
+    'sex', 'twin', 'b.marr', 'mom.lths', 'mom.hs', 'mom.scoll', 'cig',
+    'first', 'booze', 'drugs', 'work.dur', 'prenatal',
+    'ark', 'ein', 'har', 'mia', 'pen', 'tex', 'was',
+    'y_A_sim', 'y_B_sim'
+]
+
+def test_load_ihdp_simulated_outcomes_dataset():
+    df = load_ihdp_simulated_outcomes_dataset()
+    assert len(df) > 0
+    assert len(df.columns) == len(CSV_COLUMNS)
+    for column in CSV_COLUMNS:
+        assert column in df.columns
---
+++ test/datasets/test_lalonde_dataset.py
@@ -0,0 +1,14 @@
+from pycausal_explorer.datasets.lalonde_nsw_jobs import load_lalonde_nsw_jobs_dataset
+
+
+CSV_COLUMNS = [
+    'data_id', 'treat', 'age', 'education', 'married',
+    'nodegree', 'black', 'hispanic', 'white', 're75', 're78'
+]
+
+def test_load_lalonde_nsw_jobs_dataset():
+    df = load_lalonde_nsw_jobs_dataset()
+    assert len(df) > 0
+    assert len(df.columns) == len(CSV_COLUMNS)
+    for column in CSV_COLUMNS:
+        assert column in df.columns
