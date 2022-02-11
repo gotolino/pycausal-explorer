c7f07e79f586139fd8ce4660f510017992281563
joao-vta <joaovta7@gmail.com>
2022-02-11 18:58:01 -0300

    fix: Fixed comparision for emptiness. Should work now.

--- pycausal_explorer/tlearner/_two_learner.py
+++ pycausal_explorer/tlearner/_two_learner.py
@@ -45,9 +45,9 @@ class TLearner(BaseCausalModel):
         check_is_fitted(self)
         pred = np.copy(X)
 
-        if not X[w == 1].empty:
+        if X[w == 1].size != 0:
             pred[w == 1] = self.fitted_treatment_model.predict(X[w == 1]).reshape(-1, 1)
-        if not X[w == 0].empty:
+        if X[w == 0].size != 0:
             pred[w == 0] = self.fitted_control_model.predict(X[w == 0]).reshape(-1, 1)
 
         return pred
