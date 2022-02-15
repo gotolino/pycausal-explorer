721fb9e8d1a969aa54c5cec53ad06632f52e14dd
joao-vta <joaovta7@gmail.com>
2022-02-15 09:37:50 -0300

    feat: Added logistic Tlearner

--- pycausal_explorer/meta/__init__.py
+++ pycausal_explorer/meta/__init__.py
@@ -1,2 +1,2 @@
 from ._xlearner import XLearner
-from ._tlearner import TLearnerRegressor
+from ._tlearner import TLearnerRegressor, TLearnerLogistic
--- pycausal_explorer/meta/_tlearner.py
+++ pycausal_explorer/meta/_tlearner.py
@@ -9,8 +9,8 @@ class BaseTLearner(BaseCausalModel):
     """
     Implementation of the two learner model, also known as grouped conditional outcome model (GCOM).
 
-    Uses a provided model to predict outcome under treatment, and another model to predict outcome when
-    not under treatment.
+    Uses a provided model to predict outcome under treatment, and another model
+    when not under treatment.
     Uses both models to estimate treatment effect.
 
     Parameters
@@ -42,18 +42,6 @@ class BaseTLearner(BaseCausalModel):
         self.is_fitted_ = True
         return self
 
-    def predict_ite(self, X):
-        check_is_fitted(self)
-        return self.predict(X, np.ones(shape=X.shape[0])) - self.predict(
-            X, np.zeros(shape=X.shape[0])
-        )
-
-
-class TLearnerRegressor(BaseTLearner):
-    def __init__(self, treatment_learner, control_learner):
-        super().__init__(treatment_learner, control_learner)
-        self._estimator_type = "regressor"
-
     def predict(self, X, w):
         check_is_fitted(self)
         predictions = np.empty(shape=[X.shape[0], 1])
@@ -66,3 +54,78 @@ class TLearnerRegressor(BaseTLearner):
             predictions[w == 0] = self.control_learner.predict(X[w == 0]).reshape(-1, 1)
 
         return predictions
+
+
+class TLearnerLogistic(BaseTLearner):
+    """
+    Implementation of the two learner model, also known as grouped conditional outcome model (GCOM).
+
+    Logistic version. Should be used with binary outcome data.
+
+    Uses a provided model to predict probability of outcome under treatment, and another model
+    whe not under treatment.
+    Uses both models to estimate treatment effect.
+
+    Parameters
+    ----------
+    treatment_learner: estimator object
+        base learner for samples under treatment.
+
+    control_learner: estimator object
+        base learner for samples not under treatment.
+    """
+
+    def __init__(self, treatment_learner, control_learner):
+        super().__init__(treatment_learner, control_learner)
+        self._estimator_type = "classifier"
+
+    def predict_proba(self, X, w):
+        check_is_fitted(self)
+        predictions = np.empty(shape=[X.shape[0], 1])
+
+        if 1 in w:
+            predictions[w == 1] = self.treatment_learner.predict_proba(X[w == 1])[
+                :, 1
+            ].reshape(-1, 1)
+        if 0 in w:
+            predictions[w == 0] = self.control_learner.predict_proba(X[w == 0])[
+                :, 1
+            ].reshape(-1, 1)
+
+        return predictions
+
+    def predict_ite(self, X):
+        check_is_fitted(self)
+        return self.predict_proba(X, np.ones(shape=X.shape[0])) - self.predict_proba(
+            X, np.zeros(shape=X.shape[0])
+        )
+
+
+class TLearnerRegressor(BaseTLearner):
+    """
+    Implementation of the two learner model, also known as grouped conditional outcome model (GCOM).
+
+    Regressor version. Should be used with continuous outcome data.
+
+    Uses a provided model to predict outcome under treatment, and another model to predict outcome when
+    not under treatment.
+    Uses both models to estimate treatment effect.
+
+    Parameters
+    ----------
+    treatment_learner: estimator object
+        base learner for samples under treatment.
+
+    control_learner: estimator object
+        base learner for samples not under treatment.
+    """
+
+    def __init__(self, treatment_learner, control_learner):
+        super().__init__(treatment_learner, control_learner)
+        self._estimator_type = "regressor"
+
+    def predict_ite(self, X):
+        check_is_fitted(self)
+        return self.predict(X, np.ones(shape=X.shape[0])) - self.predict(
+            X, np.zeros(shape=X.shape[0])
+        )
