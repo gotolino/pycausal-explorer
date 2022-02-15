317e8363db5cade52de823eccf1eebf8f4ec6717
joao-vta <joaovta7@gmail.com>
2022-02-15 09:46:51 -0300

    test: Updated old tests and added new ones

--- test/meta/test_tlearner.py
+++ test/meta/test_tlearner.py
@@ -1,9 +1,9 @@
 import pytest
 
 from pycausal_explorer.datasets.synthetic import create_synthetic_data
-from pycausal_explorer.meta import TLearner
+from pycausal_explorer.meta import TLearnerRegressor, TLearnerLogistic
 
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.base import BaseEstimator
 
@@ -11,7 +11,7 @@ from sklearn.preprocessing import PolynomialFeatures
 
 
 def test_tlearner_init():
-    tlearner = TLearner(LinearRegression(), LinearRegression())
+    tlearner = TLearnerRegressor(LinearRegression(), LinearRegression())
     assert isinstance(tlearner.treatment_learner, BaseEstimator)
     assert isinstance(tlearner.control_learner, BaseEstimator)
 
@@ -19,7 +19,7 @@ def test_tlearner_init():
 def test_tlearner_train_linear():
     x, w, y = create_synthetic_data(random_seed=42)
 
-    tlearner = TLearner(LinearRegression(), LinearRegression())
+    tlearner = TLearnerRegressor(LinearRegression(), LinearRegression())
 
     tlearner.fit(x, y, treatment=w)
     _ = tlearner.predict(x, w)
@@ -27,11 +27,23 @@ def test_tlearner_train_linear():
     model_ate = tlearner.predict_ate(x)
     assert 1.0 == pytest.approx(model_ate, 0.0001)
 
+def test_tlearner_train_logistic():
+    x, w, y = create_synthetic_data(random_seed=42, target_type="categorical")
+
+    tlearner = TLearnerLogistic(LogisticRegression(), LogisticRegression())
+
+    tlearner.fit(x, y, treatment=w)
+    _ = tlearner.predict(x, w)
+    _ = tlearner.predict_ite(x)
+    model_ate = tlearner.predict_ate(x)
+
+    # Logistic models are not as good as regressors :(
+    assert model_ate > 0.5
 
 def test_tlearner_train_forest():
     x, w, y = create_synthetic_data(random_seed=42)
 
-    tlearner = TLearner(RandomForestRegressor(), RandomForestRegressor())
+    tlearner = TLearnerRegressor(RandomForestRegressor(), RandomForestRegressor())
 
     tlearner.fit(x, y, treatment=w)
     _ = tlearner.predict(x, w)
@@ -48,7 +60,7 @@ def test_tlearner_train_polynomial():
     poly = PolynomialFeatures(degree=2, include_bias=False)
     poly_features = poly.fit_transform(x.reshape(-1, 1))
 
-    tlearner = TLearner(LinearRegression(), LinearRegression())
+    tlearner = TLearnerRegressor(LinearRegression(), LinearRegression())
     tlearner.fit(poly_features, y, treatment=w)
 
     model_ate = tlearner.predict_ate(poly_features)
