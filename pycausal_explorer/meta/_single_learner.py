2ab1819df768c279c021d2aa253664042d3f3f42
joao-vta <joaovta7@gmail.com>
2022-02-15 14:53:32 -0300

    feat: Implemented slearner logistic

--- pycausal_explorer/meta/_single_learner.py
+++ pycausal_explorer/meta/_single_learner.py
@@ -5,19 +5,7 @@ from sklearn.utils.validation import check_is_fitted, check_X_y
 from pycausal_explorer.base import BaseCausalModel
 
 
-class SingleLearner(BaseCausalModel):
-    """
-    Implementation of the single learner model.
-
-    Uses a single provided model to predict outcome when under treatment, and when not.
-    Uses that to estimate treatment effect.
-
-    Parameters
-    ----------
-    learner: estimator object
-        base learner to use when predicting outcome. Should implement fit and predict methods.
-    """
-
+class SingleLearnerBase(BaseCausalModel):
     def __init__(self, learner):
         if isinstance(learner, type):
             raise ValueError(
@@ -37,8 +25,57 @@ class SingleLearner(BaseCausalModel):
         check_is_fitted(self)
         return self.learner.predict(np.column_stack([X, w]))
 
+
+class SingleLearnerRegressor(SingleLearnerBase):
+    """
+    Implementation of the single learner model.
+
+    Regressor version, should be used with continuous data.
+
+    Uses a single provided model to predict outcome when under treatment, and when not.
+    Uses that to estimate treatment effect.
+
+    Parameters
+    ----------
+    learner: estimator object
+        base learner to use when predicting outcome. Should implement fit and predict methods.
+    """
+
+    def __init__(self, learner):
+        super().__init__()
+        self._estimator_type = "regressor"
+
     def predict_ite(self, X):
         check_is_fitted(self)
         return self.predict(X, np.ones(shape=X.shape[0])) - self.predict(
+            X, np.zeros(shape=X.shape[0])
+        )
+
+
+class SingleLearnerLogistic(SingleLearnerBase):
+    """
+    Implementation of the single learner model.
+
+    Logistic version, should be used with binary data.
+
+    Uses a single provided model to predict outcome when under treatment, and when not.
+    Uses that to estimate treatment effect.
+
+    Parameters
+    ----------
+    learner: estimator object
+        base learner to use when predicting outcome. Should implement fit and predict methods.
+    """
+
+    def __init__(self, learner):
+        super().__init__(learner)
+        self._estimator_type = "classifier"
+
+    def predict_proba(self, X, w):
+        return self.learner.predict_proba(np.column_stack([X, w]))[:, 1]
+
+    def predict_ite(self, X):
+        check_is_fitted(self)
+        return self.predict_proba(X, np.ones(shape=X.shape[0])) - self.predict_proba(
             X, np.zeros(shape=X.shape[0])
         )
