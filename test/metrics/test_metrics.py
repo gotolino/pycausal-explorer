345318b13d72f25d5115dfd47c01fcf9226ba7d3
gotolino <guilhermegoto89@gmail.com>
2024-01-26 23:55:47 -0300

    test:tests

--- test/metrics/test_metrics.py
+++ test/metrics/test_metrics.py
@@ -4,13 +4,13 @@ import pytest
 from pycausal_explorer.metrics import mape, pehe
 
 def test_mape_metric():
-    x = np.array([1, 1, 2])
-    y = np.array([1, 1, 1])
+    x = np.array([1, 1, 1])
+    y = np.array([1, 1, 4])
     result = mape(x, y)
     assert result == 1
 
 def test_pehe_metric():
-    x = np.array([1, 1])
-    y = np.array([1, 5])
+    x = np.array([1, 1, 1, 5])
+    y = np.array([1, 1, 1, 1])
     result = pehe(x, y)
     assert result == 2
